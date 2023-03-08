45c2dccef95a615cbbfa9beb93a99af9d2668029
Shrawan Kr Sharma <shrawan25.sharma@gmail.com>
2023-03-08 22:09:36 +0530

    Added user unit test case for checking full address

--- UserDemoTests/UserDemoTests.swift
+++ UserDemoTests/UserDemoTests.swift
@@ -10,8 +10,11 @@ import XCTest
 
 final class UserDemoTests: XCTestCase {
 
+    var user:User!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        user = User(id: 1212, username: "Username", name: "Password", email: "shrawan25.sharma@gmail.com", website: "https://www.google.com", phone: "+918812312", company: Company(name: "Google", bs: "bs", catchPhrase: "Catch phrase"), address: Address(street: "Street", city: "City", zipCode: "12312", suite: "suite", geo: GeoLocation(lat: "12.2", lng: "22.4")))
     }
 
     override func tearDownWithError() throws {
@@ -24,6 +27,8 @@ final class UserDemoTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertFalse(user.address.getFullAddress() == "")
+        XCTAssertTrue(user.address.getFullAddress() == "Street City 12312 suite")
     }
 
     func testPerformanceExample() throws {
